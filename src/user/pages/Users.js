cc77dd57015c503c1c4e838145436d67f7960a32
Soltane <soltanne92@gmail.com>
2020-04-14 16:31:18 -0500

    Users List

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,7 +2,14 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    return <UsersList />;
+    const USERS =[{
+      id: 'u1',
+      name:'soltane',
+      image:'https://www.pexels.com/photo/america-architecture-bay-boat-208745/',
+      places:3
+    }
+  ];
+      return <UsersList items={USERS}/>;
 }
  
 export default Users;
