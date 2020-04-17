47bbc0b303056900a2bf1059ed22a656a37627cf
Soltane <soltanne92@gmail.com>
2020-04-17 12:00:23 -0500

    Adjusting the Input Component

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -7,6 +7,8 @@ import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
 } from '../../shared/util/validators';
+import './PlaceForm.css';
+
 
 const DUMMY_PLACES = [
   {
@@ -51,7 +53,7 @@ const UpdatePlace = () => {
   }
 
   return (
-    <form>
+    <form className="place-form">
       <Input
         id="title"
         element="input"
