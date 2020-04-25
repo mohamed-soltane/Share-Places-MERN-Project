7c2614890c0dbb74c7150953ee1e25ce0e0e655b
Soltane <soltanne92@gmail.com>
2020-04-25 01:20:29 -0500

    Sending a POST Request to the Backend

--- client/src/user/pages/Auth.js
+++ client/src/user/pages/Auth.js
@@ -54,11 +54,34 @@ const Auth = () => {
     setIsLoginMode(prevMode => !prevMode);
   };
 
-  const authSubmitHandler = event => {
-    event.preventDefault();
-    console.log(formState.inputs);
+    const authSubmitHandler = async event => {
+      event.preventDefault();
+      
+      if (isLoginMode) {
+
+      } else {
+        try {
+          const response = await fetch('http://localhost:5000/api/users/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+          const responseData = await response.json();
+          console.log(responseData);
+    }   catch (err) {
+        console.log(err);
+    }
+  }
     auth.login();
-  };
+};
+
 
   return (
     <Card className="authentication">
